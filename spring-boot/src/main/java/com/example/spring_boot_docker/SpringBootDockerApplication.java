c800e65516cd31603ed2e47da7b0786865724f60
devero11 <casavaspun@gmail.com>
2025-11-01 11:26:26 +0200

    added docker-compose for postgres

--- spring-boot/src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
+++ spring-boot/src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -1,21 +1,36 @@
+
 package com.example.spring_boot_docker;
- 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 public class SpringBootDockerApplication {
- 
-        @RequestMapping("/")
-        public String home() {
-                return "Hello World!";
-        }
- 
-        public static void main(String[] args) {
-                SpringApplication.run(SpringBootDockerApplication.class, args);
-        }
- 
+
+    private final JdbcTemplate jdbcTemplate;
+
+    // Constructor must match class name
+    public SpringBootDockerApplication(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/hello")
+    public String getHello() {
+        String result = jdbcTemplate.queryForObject(
+            "SELECT name FROM test_table LIMIT 1",
+            String.class
+        );
+        return "Value from DB: " + result;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 }
+
